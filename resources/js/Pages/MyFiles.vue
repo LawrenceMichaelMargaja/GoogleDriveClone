38521520f053cc5f127b1e5a51ff1aa389875daf
lawrence michael margaja <lawrencemichaelmargaja@lawrences-MacBook-Air.local>
2023-12-11 11:17:28 +0800

    bread crumbs ui added.

--- resources/js/Pages/MyFiles.vue
+++ resources/js/Pages/MyFiles.vue
@@ -1,6 +1,30 @@
 <template>
     <AuthenticatedLayout>
-        <pre>{{ancestors}}</pre>
+        <nav class="flex items-center justify-between p-1 mb-3">
+            <ol class="inline-flex items-center space-x-1 md:space-x-3">
+                <li v-for="ans of ancestors.data" :key="ans.id" class="inline-flex items-center">
+                    <Link v-if="!ans.parent_id" :href="route('myFiles')"
+                        class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-black"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                        </svg>
+                        My Files
+                    </Link>
+                    <div v-else class="flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                        </svg>
+
+                        <Link :href="route('myFiles', {folder: ans.path})"
+                              class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white"
+                        >
+                            {{ans.name}}
+                        </Link>
+                    </div>
+                </li>
+            </ol>
+        </nav>
         <table v-if="files.data.length" class="min-w-full">
             <thead class="bg-gray-100 border-b">
             <tr>
@@ -56,6 +80,7 @@
 <script setup>
 
 // Imports
+import {Link} from "@inertiajs/vue3";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import {router} from "@inertiajs/vue3";
 
